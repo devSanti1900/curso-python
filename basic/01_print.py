de8496b687d5e76d5110b1b5ea89676d7123a4f3
Carlos Santiago <santiagosigalac@gmail.com>
2025-07-24 11:03:20 -0600

    se avanza en curso, se ve algunos tipos de datos,y se crea una carpeta para tener un mejor orden en los archivos

---
+++ basic/01_print.py
@@ -0,0 +1,4 @@
+print("Hello World")
+print('Tambien funciona con comillas simples')
+print("Python","es","genial")
+print("Python","es","genial", sep="-")
---
+++ basic/02_types.py
@@ -0,0 +1,10 @@
+"""
+    python tiene varios tipos de datos
+    int,float,bool,complex, str, NoneType,list, tuples, dict, range, set...
+
+"""
+
+print(type(10))
+print(type(10.2))
+print(type("hola"))
+print(type(None))
